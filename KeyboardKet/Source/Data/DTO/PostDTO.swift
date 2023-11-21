1d0ac25fe3de8f137f746b41ffb69cf91e76bd87
Taeyoung Won <hexter1994@gmail.com>
2023-11-22 03:20:50 +0900

    [Add] #2 PostDTO 정의

---
+++ KeyboardKet/Source/Data/DTO/PostDTO.swift
@@ -0,0 +1,19 @@
+//
+//  PostDTO.swift
+//  KeyboardKet
+//
+//  Created by 원태영 on 11/22/23.
+//
+
+import Foundation
+
+struct PostDTO {
+    // MARK: - Property
+    let id: String
+    let title: String
+    let content: String
+    let createAt: Date
+    let updateAt: Date
+    let productImages: [String]
+    let status: String
+}
