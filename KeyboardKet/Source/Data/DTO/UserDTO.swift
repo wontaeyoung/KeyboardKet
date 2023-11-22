a9b5627c1586d814d9b81ac0ec19ad49dc0efecb
Taeyoung Won <hexter1994@gmail.com>
2023-11-22 23:52:59 +0900

    [Feat] #2 toModel 매핑 함수 구현

--- KeyboardKet/Source/Data/DTO/UserDTO.swift
+++ KeyboardKet/Source/Data/DTO/UserDTO.swift
@@ -7,11 +7,22 @@
 
 import Foundation
 
-struct UserDTO {
+struct UserDTO: DTO {
   // MARK: - Property
   let id: String
   let nickname: String
   let profileImage: String
   let postIDs: [String]
   let createAt: Date
+  
+  // MARK: - Mapping
+  func toModel() -> Model {
+    return User(
+      id: UUID(uuidString: id) ?? UUID(),
+      nickname: nickname,
+      profileImage: URL(string: profileImage),
+      postIDs: postIDs,
+      createAt: createAt
+    )
+  }
 }
