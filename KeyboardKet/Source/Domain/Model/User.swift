16dbcf871599fa51105f645036cec25c83167f03
Taeyoung Won <hexter1994@gmail.com>
2023-11-22 23:53:09 +0900

    [Feat] #2 toDTO 매핑 함수 구현

--- KeyboardKet/Source/Domain/Model/User.swift
+++ KeyboardKet/Source/Domain/Model/User.swift
@@ -7,11 +7,22 @@
 
 import Foundation
 
-struct User {
+struct User: Model {
   // MARK: - Property
   let id: UUID
   let nickname: String
   let profileImage: URL?
   let postIDs: [String]
   let createAt: Date
+  
+  // MARK: - Mapping
+  func toDTO() -> DTO {
+    return UserDTO(
+      id: id.uuidString,
+      nickname: nickname,
+      profileImage: profileImage?.description ?? "",
+      postIDs: postIDs,
+      createAt: createAt
+    )
+  }
 }
