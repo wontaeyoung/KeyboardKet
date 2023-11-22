104cedc3056616f8c36eb6d7795ed11aabe7df43
Taeyoung Won <hexter1994@gmail.com>
2023-11-22 23:48:22 +0900

    [Add] #2 User 모델 정의

---
+++ KeyboardKet/Source/Domain/Model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  KeyboardKet
+//
+//  Created by 원태영 on 11/22/23.
+//
+
+import Foundation
+
+struct User {
+  // MARK: - Property
+  let id: String
+  let nickname: String
+  let profileImage: URL?
+  let postIDs: [String]
+  let createAt: Date
+  let updateAt: Date
+}
