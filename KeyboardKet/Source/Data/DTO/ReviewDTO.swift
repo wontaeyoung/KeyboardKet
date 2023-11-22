ff76c397a23bec2e622349bfc2c86ff44d190f4b
Taeyoung Won <hexter1994@gmail.com>
2023-11-23 03:02:08 +0900

    [Add] #2 ReviewDTO 정의

---
+++ KeyboardKet/Source/Data/DTO/ReviewDTO.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewDTO.swift
+//  KeyboardKet
+//
+//  Created by 원태영 on 11/23/23.
+//
+
+import Foundation
+
+struct ReviewDTO {
+  // MARK: - Property
+  let id: String
+  let postID: String
+  let traderID: String
+  let tradeType: String
+  let createAt: Date
+}
