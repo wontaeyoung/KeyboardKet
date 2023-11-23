05636662a43a8c1c4181c3ec1588fc1ee80322af
Taeyoung Won <hexter1994@gmail.com>
2023-11-23 18:07:04 +0900

    [Feat] #2 ReviewDTO toModel 매핑 함수 구현

--- KeyboardKet/Source/Data/DTO/ReviewDTO.swift
+++ KeyboardKet/Source/Data/DTO/ReviewDTO.swift
@@ -7,11 +7,30 @@
 
 import Foundation
 
-struct ReviewDTO {
+struct ReviewDTO: DTO {
   // MARK: - Property
   let id: String
   let postID: String
   let traderID: String
   let tradeType: Review.TradeType
   let createAt: Date
+  
+  // MARK: - Mapping
+  func toModel() throws -> Model {
+    guard 
+      let uuid = UUID(uuidString: id),
+      let postUUID = UUID(uuidString: postID),
+      let traderUUID = UUID(uuidString: traderID)
+    else {
+      throw DTOError.mapToModelFailed
+    }
+    
+    return Review(
+      id: uuid,
+      postID: postUUID,
+      traderID: traderUUID,
+      tradeType: tradeType,
+      createAt: createAt
+    )
+  }
 }
