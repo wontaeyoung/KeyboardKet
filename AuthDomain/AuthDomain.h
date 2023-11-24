91ae2e0057129fa681e627771a17be953ddf2b2f
Taeyoung Won <hexter1994@gmail.com>
2023-11-24 23:59:42 +0900

    [Add] #7 AuthDomain 모듈 추가

---
+++ AuthDomain/AuthDomain.h
@@ -0,0 +1,18 @@
+//
+//  AuthDomain.h
+//  AuthDomain
+//
+//  Created by 원태영 on 11/24/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AuthDomain.
+FOUNDATION_EXPORT double AuthDomainVersionNumber;
+
+//! Project version string for AuthDomain.
+FOUNDATION_EXPORT const unsigned char AuthDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuthDomain/PublicHeader.h>
+
+
