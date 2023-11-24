8aecd4c9f4db9d8720dd0cbaa59059603a093089
Taeyoung Won <hexter1994@gmail.com>
2023-11-25 01:01:54 +0900

    [Add] #7 PostDomain 모듈 추가

---
+++ PostDomain/PostDomain.h
@@ -0,0 +1,18 @@
+//
+//  PostDomain.h
+//  PostDomain
+//
+//  Created by 원태영 on 11/25/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PostDomain.
+FOUNDATION_EXPORT double PostDomainVersionNumber;
+
+//! Project version string for PostDomain.
+FOUNDATION_EXPORT const unsigned char PostDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PostDomain/PublicHeader.h>
+
+
