346934449c0ad0e09795ac2e1897406c02dd9f9e
Taeyoung Won <hexter1994@gmail.com>
2023-11-25 01:35:44 +0900

    [Add] #7 DomainCore 모듈 추가

---
+++ DomainCore/DomainCore.h
@@ -0,0 +1,18 @@
+//
+//  DomainCore.h
+//  DomainCore
+//
+//  Created by 원태영 on 11/25/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DomainCore.
+FOUNDATION_EXPORT double DomainCoreVersionNumber;
+
+//! Project version string for DomainCore.
+FOUNDATION_EXPORT const unsigned char DomainCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DomainCore/PublicHeader.h>
+
+
