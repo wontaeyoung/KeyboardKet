129d253909c3f88e4bd43651c99a3946a6aa3d13
Taeyoung Won <hexter1994@gmail.com>
2023-11-27 00:17:21 +0900

    [Feat] #9 AppError 프로토콜 채택, 케이스별 안내 설명 문자열 작성

--- FirestoreService/Definition/Error/FirestoreError.swift
+++ FirestoreService/Definition/Error/FirestoreError.swift
@@ -5,6 +5,7 @@
 //  Created by 원태영 on 11/27/23.
 //
 
+import Core
 import FirebaseFirestore
 
 enum FirestoreError {
@@ -14,4 +15,28 @@ enum FirestoreError {
   case emptyCollection
   case emptyQuery
   case unknown
+}
+
+extension FirestoreError: AppError {
+  var errorDescription: String {
+    switch self {
+      case .encodeError:
+        return "인코딩에 실패했습니다."
+        
+      case .decodeError:
+        return "디코딩에 실패했습니다."
+        
+      case .noDocument(let document):
+        return "\(document.documentID) 문서를 찾을 수 없습니다."
+        
+      case .emptyCollection:
+        return "빈 컬렉션입니다."
+        
+      case .emptyQuery:
+        return "쿼리에 해당하는 문서를 찾을 수 없습니다."
+        
+      case .unknown:
+        return "정의되지 않은 오류입니다."
+    }
+  }
 }
