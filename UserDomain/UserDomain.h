0ef8e5861abc3ebfa58ba987bbcaae0341ba1c8e
Taeyoung Won <hexter1994@gmail.com>
2023-11-26 22:46:51 +0900

    [Add] #7 UserDomain 모듈 추가

---
+++ UserDomain/UserDomain.h
@@ -0,0 +1,18 @@
+//
+//  UserDomain.h
+//  UserDomain
+//
+//  Created by 원태영 on 11/26/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UserDomain.
+FOUNDATION_EXPORT double UserDomainVersionNumber;
+
+//! Project version string for UserDomain.
+FOUNDATION_EXPORT const unsigned char UserDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserDomain/PublicHeader.h>
+
+
