e6f0fd555aefd3267d1de9e3b41be829372a01a3
Taeyoung Won <hexter1994@gmail.com>
2023-11-26 23:51:53 +0900

    [Build] #9 FirebaseStroageService 모듈 추가

---
+++ FirebaseStroageService/FirebaseStroageService.h
@@ -0,0 +1,18 @@
+//
+//  FirebaseStroageService.h
+//  FirebaseStroageService
+//
+//  Created by 원태영 on 11/26/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirebaseStroageService.
+FOUNDATION_EXPORT double FirebaseStroageServiceVersionNumber;
+
+//! Project version string for FirebaseStroageService.
+FOUNDATION_EXPORT const unsigned char FirebaseStroageServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseStroageService/PublicHeader.h>
+
+
