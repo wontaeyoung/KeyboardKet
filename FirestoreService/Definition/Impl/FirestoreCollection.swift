716a2beb902a5a6534f593ac183b8a87dfd99417
Taeyoung Won <hexter1994@gmail.com>
2023-11-27 00:49:49 +0900

    [Feat] #9 Firestore Collection 열거형 정의

---
+++ FirestoreService/Definition/Impl/FirestoreCollection.swift
@@ -0,0 +1,12 @@
+//
+//  FirestoreCollection.swift
+//  KeyboardKet
+//
+//  Created by 원태영 on 11/27/23.
+//
+
+enum FirestoreCollection: String, FirestoreSchema {
+  case User
+  case Post
+  case Review
+}
