b8dcfa4800d49cca47a421a488cbf4332c6feb8d
Taeyoung Won <hexter1994@gmail.com>
2023-12-02 01:51:20 +0900

    [Add] #11 DTO 변환 실패 에러 케이스 정의

--- KeyboardKet/KeyboardKet/Source/Data/DTO/Error/DTOError.swift
+++ KeyboardKet/KeyboardKet/Source/Data/DTO/Error/DTOError.swift
@@ -7,11 +7,15 @@
 
 enum DTOError: AppError {
   case mapToModelFailed
+  case mapToDTOFailed
   
   var errorDescription: String {
     switch self {
       case .mapToModelFailed:
         return "클라이언트 데이터로 변경에 실패했습니다."
+        
+      case .mapToDTOFailed:
+        return "전송 데이터 변경에 실패했습니다."
     }
   }
 }
