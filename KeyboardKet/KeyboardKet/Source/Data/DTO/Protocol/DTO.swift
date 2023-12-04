8e2792cf05194747c7d1cf1457e9ef29c80a0c4b
Taeyoung Won <hexter1994@gmail.com>
2023-12-05 01:29:05 +0900

    [Feat] #14 구조체를 딕셔너리로 변환하는 asDictionary 구현

--- KeyboardKet/KeyboardKet/Source/Data/DTO/Protocol/DTO.swift
+++ KeyboardKet/KeyboardKet/Source/Data/DTO/Protocol/DTO.swift
@@ -8,3 +8,18 @@
 protocol DTO: Codable, Identifiable {
   func toModel() throws -> Model
 }
+
+extension DTO {
+  var asDictionary: [String: Any] {
+    let mirror: Mirror = Mirror(reflecting: self)
+    var dictionary: [String: Any] = [:]
+    
+    mirror.children.forEach { property in
+      guard let key = property.label else { return }
+      
+      dictionary.updateValue(property.value, forKey: key)
+    }
+    
+    return dictionary
+  }
+}
