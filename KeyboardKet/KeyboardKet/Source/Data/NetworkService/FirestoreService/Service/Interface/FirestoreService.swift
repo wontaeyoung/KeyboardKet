bde2503449b1dc0644b6d3b3b8275f3fa5f9fc91
Taeyoung Won <hexter1994@gmail.com>
2023-12-04 21:37:45 +0900

    [Del] #14 FirestoreService 기존 인터페이스 함수 삭제

--- KeyboardKet/KeyboardKet/Source/Data/NetworkService/FirestoreService/Service/Interface/FirestoreService.swift
+++ KeyboardKet/KeyboardKet/Source/Data/NetworkService/FirestoreService/Service/Interface/FirestoreService.swift
@@ -10,46 +10,8 @@ import FirebaseFirestore
 protocol FirestoreService {
   associatedtype FirestoreField = any FirestoreFieldProtocol
   
-  // MARK: - Path
-  func getCollectionPath(col collection: FirestoreCollection) -> CollectionReference
-  
-  func getDocumentPath(
-    col collection: FirestoreCollection,
-    docID documentID: String
-  ) -> DocumentReference
-  
-  func getCollectionPath(
-    supCol superCollection: FirestoreCollection,
-    supDocID superDocumentID: String,
-    subCol subCollection: FirestoreCollection
-  ) -> CollectionReference
-  
-  func getDocumentPath(
-    supCol superCollection: FirestoreCollection,
-    supDocID superDocumentID: String,
-    subCol subCollection: FirestoreCollection,
-    subDocID subDocumentID: String
-  ) -> DocumentReference
-  
-  // MARK: - Create
-  func addDocument<T: DTO>(
-    docPath documentPath: DocumentReference,
-    dto: T
-  ) throws
-  
   func create<T: DTO>(
     col: FirestoreCollection,
     dto: T
   ) throws
-  
-  // MARK: - Read
-  func getDocument(docPath documentPath: DocumentReference) async throws -> DocumentSnapshot
-  
-  func getDocuments(colPath collectionPath: CollectionReference) async throws -> [QueryDocumentSnapshot]
-  
-  func getDocuments(
-    colPath collectionPath: CollectionReference,
-    field: FirestoreField,
-    operation: FirestoreQueryOperation
-  ) async throws -> [QueryDocumentSnapshot]
 }
