38b4a1d518c3d83c971bcd6a528dfd5c63a205de
Taeyoung Won <hexter1994@gmail.com>
2023-12-08 00:46:05 +0900

    [Feat] #17 FirebaseAuthService 인터페이스 함수 정의

---
+++ KeyboardKet/KeyboardKet/Source/Data/NetworkService/FirebaseAuthService/Service/Interface/FirebaseAuthService.swift
@@ -0,0 +1,20 @@
+//
+//  FirebaseAuthService.swift
+//  KeyboardKet
+//
+//  Created by 원태영 on 12/8/23.
+//
+
+protocol FirebaseAuthService {
+  func signUp(
+    email: String,
+    password: String
+  ) async throws
+  
+  func signIn(
+    email: String,
+    password: String
+  ) async throws
+  
+  func signOut() async throws
+}
