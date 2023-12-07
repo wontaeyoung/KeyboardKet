e28cd0500fc96531251202cccfa4f51bdba235fd
Taeyoung Won <hexter1994@gmail.com>
2023-12-08 07:10:33 +0900

    [Feat] #17 signUp 메서드 상세로직 구현

--- KeyboardKet/KeyboardKet/Source/Data/NetworkService/FirebaseAuthService/Service/Impl/FirebaseAuthServiceImpl.swift
+++ KeyboardKet/KeyboardKet/Source/Data/NetworkService/FirebaseAuthService/Service/Impl/FirebaseAuthServiceImpl.swift
@@ -10,4 +10,27 @@ import FirebaseAuth
 final class FirebaseAuthServiceImpl {
   // MARK: - Property
   private let auth: Auth = .auth()
+  
+  // MARK: - Method
+  func signUp(
+    email: String,
+    password: String
+  ) async throws {
+    do {
+      try await auth.createUser(
+        withEmail: email,
+        password: password
+      )
+    } catch let nsError as NSError {
+      let errorCode: AuthErrorCode.Code = AuthErrorCode(_nsError: nsError).code
+      
+      switch errorCode {
+        case .networkError:
+          throw FirebaseAuthError.networkError
+          
+        default:
+          throw FirebaseAuthError.unknown
+      }
+    }
+  }
 }
