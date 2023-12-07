5b7eef29cd43a4f7d5a65b8e34b50a2b37118972
Taeyoung Won <hexter1994@gmail.com>
2023-12-08 07:09:58 +0900

    [Feat] #17 FirebaseAuthService 구현체 추가

---
+++ KeyboardKet/KeyboardKet/Source/Data/NetworkService/FirebaseAuthService/Service/Impl/FirebaseAuthServiceImpl.swift
@@ -0,0 +1,13 @@
+//
+//  FirebaseAuthServiceImpl.swift
+//  KeyboardKet
+//
+//  Created by 원태영 on 12/8/23.
+//
+
+import FirebaseAuth
+
+final class FirebaseAuthServiceImpl {
+  // MARK: - Property
+  private let auth: Auth = .auth()
+}
